913e39650a41f7e16dfb17b41aab066609691fc8
Mohammed Mansad <mansadmohd@gmail.com>
2023-09-11 21:38:39 +0530

    this is initial commit

---
+++ src/com/abc/Sems.java
@@ -0,0 +1,12 @@
+package com.abc;
+
+public class Sems {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Just for testing the page");
+
+	}
+
+}
